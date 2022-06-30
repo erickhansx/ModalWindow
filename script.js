2cf70dc40636b779a15544f7234ad6366953b1cf
erickhansx <50700379+erickhansx@users.noreply.github.com>
2022-06-30 16:27:21 -0500

    Implement the necessary javascript to make the modal window functional on the 3 buttons

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+//Function to close the modal by adding the hidden class.
+const closeModal = () => {
+  modal.classList.add('hidden'), overlay.classList.add('hidden');
+};
 
+// Function to add the event listener to the buttons and to display the modals by removing the hidden class.
+
+const openModal = () => {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// For loop to add an eventlistener to the buttons.
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', () => {
-    console.log('Button clicked');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//Here I add the function we created to the eventlistener of the button and the overlay to close the modal by executing the function.
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
